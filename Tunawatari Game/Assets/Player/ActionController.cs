50cc9fac32de45a2ac7e47095a79e5181bb6fe01
Jintani-Koutarou-Fukuyama <gc25010@anabuki-net.ne.jp>
2026-05-20 14:04:49 +0900

    プレイヤー追加

--- Tunawatari Game/Assets/Player/ActionController.cs
+++ Tunawatari Game/Assets/Player/ActionController.cs
@@ -12,27 +12,27 @@ public class ActionController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.D))
+        if(Input.GetKey(KeyCode.S))
         {
-            transform.position += new Vector3(0.01f, 0, 0);
+            transform.position += new Vector3(0.0003f, 0, 0);
             transform.eulerAngles = new Vector3(0, 90, 0);
             playerAnim.SetBool("catwalk", true);
         }
-        else if(Input.GetKey(KeyCode.A)) 
+        else if(Input.GetKey(KeyCode.W)) 
         {
-            transform.position -= new Vector3(0.01f, 0, 0);
+            transform.position -= new Vector3(0.0003f, 0, 0);
             transform.eulerAngles = new Vector3(0, -90, 0);
             playerAnim.SetBool("catwalk", true);
         }
-        else if (Input.GetKey(KeyCode.W))
+        else if (Input.GetKey(KeyCode.D))
         {
-            transform.position += new Vector3(0, 0, 0.01f);
+            transform.position += new Vector3(0, 0, 0.0003f);
             transform.eulerAngles = new Vector3(0, 0, 0);
             playerAnim.SetBool("catwalk", true);
         }
-        else if (Input.GetKey(KeyCode.S))
+        else if (Input.GetKey(KeyCode.A))
         {
-            transform.position -= new Vector3(0, 0, 0.01f);
+            transform.position -= new Vector3(0, 0, 0.0003f);
             transform.eulerAngles = new Vector3(0, -180, 0);
             playerAnim.SetBool("catwalk", true);
         }
